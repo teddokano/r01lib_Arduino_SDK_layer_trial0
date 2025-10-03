47c14f035408ad4927f9585d40f97f752f2b4a2c
Tedd OKANO <okano.akifumi@gmail.com>
2025-10-04 06:35:39 +0900

    interrupt support added with fixing GPIO dir/mode handling

--- _r01lib_frdm_mcxa156/arduino_layer/arduino_io.cpp
+++ _r01lib_frdm_mcxa156/arduino_layer/arduino_io.cpp
@@ -13,15 +13,21 @@ std::map<int, DigitalInOut*>	digital_pins;
 
 void pin_mode( int pin_num, int mode )
 {
+	int	dir		= mode & 0x1;
+	int	pull	= mode & DigitalInOut::PullUp;
+	
 	auto	it	= digital_pins.find( pin_num );
 	
 	if ( it != digital_pins.end() )
 	{
-		(digital_pins[ pin_num ])->mode( mode );
+		if ( dir == OUTPUT )
+			(digital_pins[ pin_num ])->output();
+		else
+			(digital_pins[ pin_num ])->input();
 	}
 	else
 	{	
-		digital_pins[ pin_num ]	= new DigitalInOut( pin_num, mode );
+		digital_pins[ pin_num ]	= new DigitalInOut( pin_num, dir, 0, pull );
 
 		if ( digital_pins[ pin_num ] == nullptr )
 			panic( "error @ new, in pin_mode()" );
@@ -44,4 +50,37 @@ bool digitalRead( int pin_num )
 		return	*(digital_pins[ pin_num ]);
 	else
 		return	false;
+}
+
+void attachInterrupt( int int_num, void (*callback)(void), int mode )
+{
+	InterruptIn* int_pin	= new InterruptIn( int_num );
+
+	if ( int_pin == nullptr )
+		panic( "error @ new, in attachInterrupt()" );
+
+	switch ( mode )
+	{
+		case	RISING:
+			int_pin->rise( callback );
+			break;
+
+		case	FALLING:
+			int_pin->fall( callback );
+			break;
+
+		case	CHANGE:
+			int_pin->rise( callback );
+			int_pin->fall( callback );
+			break;
+
+		default:
+			panic( "error @ attachInterrupt(), unknown mode" );
+			break;
+	}
+}
+
+int digitalPinToInterrupt( int pin_num )
+{
+	return	pin_num;
 }
--- _r01lib_frdm_mcxn947/arduino_layer/arduino_io.h
+++ _r01lib_frdm_mcxn947/arduino_layer/arduino_io.h
@@ -7,13 +7,23 @@
 #ifndef R01LIB_ARDUINO_IO_H
 #define R01LIB_ARDUINO_IO_H
 
-constexpr int	INPUT	= DigitalInOut::INPUT;
-constexpr int	OUTPUT	= DigitalInOut::OUTPUT;
+constexpr int	INPUT			= DigitalInOut::INPUT;
+constexpr int	OUTPUT			= DigitalInOut::OUTPUT;
+constexpr int	INPUT_PULLUP	= INPUT | DigitalInOut::PullUp;
+
 constexpr bool	HIGH	= true;
 constexpr bool	LOW		= false;
 
 void	pin_mode( int pin_num, int mode );
 void	digitalWrite( int pin_num, bool state );
 bool	digitalRead( int pin_num );
+
+constexpr int	RISING	= 0;
+constexpr int	FALLING	= 1;
+constexpr int	CHANGE	= 2;
+
+void	attachInterrupt( int int_num, void (*callback)(void), int mode );
+int		digitalPinToInterrupt( int pin_num );
+
 
 #endif // !R01LIB_ARDUINO_IO_H
